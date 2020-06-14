950e49b34e844ca19913268cb206b6c623ffb8b8
Maegan Wilson <appsbymw@gmail.com>
2020-06-14 18:44:31 -0500

    Adding xcode project

---
+++ calculator/calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Maegan Wilson on 6/14/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
